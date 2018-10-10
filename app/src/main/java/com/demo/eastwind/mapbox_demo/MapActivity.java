4ebccee0a8a61483e2d2c2964785ee7a4b8a5895
Janith Anoma <janith@elegantmedia.lk>
2018-10-10 15:23:00 +0530

    my location button

--- app/src/main/java/com/demo/eastwind/mapbox_demo/MapActivity.java
+++ app/src/main/java/com/demo/eastwind/mapbox_demo/MapActivity.java
@@ -5,10 +5,11 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
-
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.location.LocationEngineListener;
 import com.mapbox.android.core.location.LocationEnginePriority;
@@ -24,160 +25,174 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
-
 import java.util.List;
 
-public class MapActivity extends AppCompatActivity implements LocationEngineListener, PermissionsListener, OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity
+    implements LocationEngineListener, PermissionsListener, OnMapReadyCallback,
+    View.OnClickListener {
 
-    private MapView mapView;
-    private PermissionsManager permissionsManager;
-    private LocationEngine locationEngine;
-    private Location originLocation;
-    private LocationLayerPlugin locationLayerPlugin;
-    private MapboxMap mapboxMap;
+  private MapView mapView;
+  private PermissionsManager permissionsManager;
+  private LocationEngine locationEngine;
+  private Location originLocation;
+  private LocationLayerPlugin locationLayerPlugin;
+  private MapboxMap mapboxMap;
+  private FloatingActionButton fMyLocation;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-        Mapbox.getInstance(this, getString(R.string.access_token));
-        mapView = findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_map);
+    Mapbox.getInstance(this, getString(R.string.access_token));
+    mapView = findViewById(R.id.mapView);
+    fMyLocation = findViewById(R.id.fbtn_my_location);
+    mapView.onCreate(savedInstanceState);
+    mapView.getMapAsync(this);
+    fMyLocation.setOnClickListener(this);
+  }
+
+  @SuppressWarnings({ "MissingPermission" })
+  private void enableLocationPlugin() {
+    // Check if permissions are enabled and if not request
+    if (PermissionsManager.areLocationPermissionsGranted(this)) {
+      initializeLocationEngine();
+      // Create an instance of the plugin. Adding in LocationLayerOptions is also an optional
+      // parameter
+      locationLayerPlugin = new LocationLayerPlugin(mapView, mapboxMap);
+      // Set the plugin's camera mode
+      locationLayerPlugin.setCameraMode(CameraMode.TRACKING);
+      locationLayerPlugin.setRenderMode(RenderMode.COMPASS);
+      getLifecycle().addObserver(locationLayerPlugin);
+    } else {
+      permissionsManager = new PermissionsManager(this);
+      permissionsManager.requestLocationPermissions(this);
     }
+  }
 
-    @SuppressWarnings({"MissingPermission"})
-    private void enableLocationPlugin() {
-        // Check if permissions are enabled and if not request
-        if (PermissionsManager.areLocationPermissionsGranted(this)) {
-            initializeLocationEngine();
-            // Create an instance of the plugin. Adding in LocationLayerOptions is also an optional
-            // parameter
-            locationLayerPlugin = new LocationLayerPlugin(mapView, mapboxMap);
+  @SuppressWarnings({ "MissingPermission" })
+  private void initializeLocationEngine() {
+    LocationEngineProvider locationEngineProvider = new LocationEngineProvider(this);
+    locationEngine = locationEngineProvider.obtainBestLocationEngineAvailable();
+    locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+    locationEngine.activate();
 
-            // Set the plugin's camera mode
-            locationLayerPlugin.setCameraMode(CameraMode.TRACKING);
-            locationLayerPlugin.setRenderMode(RenderMode.COMPASS);
-            getLifecycle().addObserver(locationLayerPlugin);
-        } else {
-            permissionsManager = new PermissionsManager(this);
-            permissionsManager.requestLocationPermissions(this);
-        }
+    Location lastLocation = locationEngine.getLastLocation();
+    if (lastLocation != null) {
+      originLocation = lastLocation;
+      setCamerpostion(originLocation);
+    } else {
+      locationEngine.addLocationEngineListener(this);
     }
+  }
 
-    @SuppressWarnings({"MissingPermission"})
-    private void initializeLocationEngine() {
-        LocationEngineProvider locationEngineProvider = new LocationEngineProvider(this);
-        locationEngine = locationEngineProvider.obtainBestLocationEngineAvailable();
-        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
-        locationEngine.activate();
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+      @NonNull int[] grantResults) {
+    permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+  }
 
-        Location lastLocation = locationEngine.getLastLocation();
-        if (lastLocation != null) {
-            originLocation = lastLocation;
-            setCamerpostion(originLocation);
-        } else {
-            locationEngine.addLocationEngineListener(this);
-        }
+  @Override
+  public void onExplanationNeeded(List<String> permissionsToExplain) {
+    Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+  }
+
+  @Override
+  public void onPermissionResult(boolean granted) {
+    if (granted) {
+      enableLocationPlugin();
+    } else {
+      Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+      finish();
     }
+  }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+  @SuppressWarnings({ "MissingPermission" })
+  @Override
+  protected void onStart() {
+    super.onStart();
+    mapView.onStart();
+    if (locationLayerPlugin != null) {
+      locationLayerPlugin.onStart();
     }
+  }
 
-    @Override
-    public void onExplanationNeeded(List<String> permissionsToExplain) {
-        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
-    }
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mapView.onResume();
+  }
 
-    @Override
-    public void onPermissionResult(boolean granted) {
-        if (granted) {
-            enableLocationPlugin();
-        } else {
-            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
-            finish();
-        }
-    }
+  @Override
+  protected void onPause() {
+    super.onPause();
+    mapView.onPause();
+  }
 
-    @SuppressWarnings({"MissingPermission"})
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mapView.onStart();
-        if (locationLayerPlugin != null) {
-            locationLayerPlugin.onStart();
-        }
+  @Override
+  protected void onStop() {
+    super.onStop();
+    mapView.onStop();
+    if (locationLayerPlugin != null) {
+      locationLayerPlugin.onStart();
     }
+  }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    mapView.onSaveInstanceState(outState);
+  }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    mapView.onDestroy();
+    locationEngine.deactivate();
+  }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mapView.onStop();
-        if (locationLayerPlugin != null) {
-            locationLayerPlugin.onStart();
-        }
-    }
+  @Override
+  public void onLowMemory() {
+    super.onLowMemory();
+    mapView.onLowMemory();
+  }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
+  @Override
+  public void onConnected() {
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+      return;
     }
+    locationEngine.requestLocationUpdates();
+  }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-        locationEngine.deactivate();
+  @Override
+  public void onLocationChanged(Location location) {
+    if (location != null) {
+      originLocation = location;
+      setCamerpostion(location);
     }
+  }
 
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
+  @Override
+  public void onMapReady(MapboxMap mapboxMap) {
+    mapboxMap.getUiSettings().setZoomControlsEnabled(true);
+    mapboxMap.getUiSettings().setZoomGesturesEnabled(true);
+    mapboxMap.getUiSettings().setScrollGesturesEnabled(true);
+    mapboxMap.getUiSettings().setAllGesturesEnabled(true);
+    this.mapboxMap = mapboxMap;
+    enableLocationPlugin();
+  }
 
-    @Override
-    public void onConnected() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        locationEngine.requestLocationUpdates();
-    }
+  void setCamerpostion(Location camerpostion) {
+    mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+        new LatLng(camerpostion.getLatitude(), camerpostion.getLongitude()), 12.0));
+  }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        if (location != null) {
-            originLocation = location;
-            setCamerpostion(location);
-        }
+  @Override public void onClick(View view) {
+    if (originLocation != null) {
+      setCamerpostion(originLocation);
     }
-
-    @Override
-    public void onMapReady(MapboxMap mapboxMap) {
-        this.mapboxMap = mapboxMap;
-        enableLocationPlugin();
-        mapboxMap.getUiSettings().setZoomControlsEnabled(true);
-        mapboxMap.getUiSettings().setZoomGesturesEnabled(true);
-        mapboxMap.getUiSettings().setScrollGesturesEnabled(true);
-        mapboxMap.getUiSettings().setAllGesturesEnabled(true);
-    }
-
-    void setCamerpostion(Location camerpostion) {
-        mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(camerpostion.getLatitude(), camerpostion.getLongitude()), 13.0));
-    }
+  }
 }
